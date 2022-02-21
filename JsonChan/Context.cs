f1b66f83b377c7ecbb15bafb3f449e384623e206
TheSnowfield <thesnowfield@moezsoft.com>
2022-02-21 15:21:04 +0800

    Add json parser

---
+++ JsonChan/Context.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+
+// ReSharper disable ConvertToAutoProperty
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable ConvertToAutoPropertyWithPrivateSetter
+
+namespace Paraparty.JsonChan
+{
+    internal class DynamicContext
+    {
+        public dynamic Value => _value;
+
+        private dynamic _value;
+        private readonly Stack<dynamic> _contextPath;
+
+        public DynamicContext()
+        {
+            _value = new ExpandoObject();
+            _contextPath = new Stack<dynamic>();
+        }
+
+        /// <summary>
+        /// Push context
+        /// </summary>
+        public void PushContext(string name)
+        {
+            var context = new ExpandoObject();
+            if (_contextPath.Count == 0) _value = context;
+
+            if (IsArrayContext())
+            {
+                PutArrayValue(context);
+                _contextPath.Push(context);
+                return;
+            }
+
+            // Create a subtree
+            if (name != string.Empty)
+                PutValue(name, context);
+
+            _contextPath.Push(context);
+        }
+
+        /// <summary>
+        /// Pop context
+        /// </summary>
+        public void PopContext()
+        {
+            if (_contextPath.Count > 1) _contextPath.Pop();
+        }
+
+        public void PushArrayContext(string name)
+        {
+            var context = new List<dynamic>();
+            if (_contextPath.Count == 0) _value = context;
+
+            if (IsArrayContext())
+            {
+                PutArrayValue(context);
+                _contextPath.Push(context);
+                return;
+            }
+
+            if (name != string.Empty)
+                PutValue(name, context);
+
+            _contextPath.Push(context);
+        }
+
+        public bool IsArrayContext()
+            => _contextPath.Peek() is List<dynamic>;
+
+        /// <summary>
+        /// Put object value
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void PutValue(string key, dynamic value)
+        {
+            if (!IsArrayContext())
+                (_contextPath.Peek() as IDictionary<string, object>)[key] = value;
+            else PutArrayValue(value);
+        }
+
+        /// <summary>
+        /// Put array value
+        /// </summary>
+        /// <param name="value"></param>
+        public void PutArrayValue(dynamic value)
+        {
+            (_contextPath.Peek() as List<dynamic>).Add(value);
+        }
+    }
+
+    internal class Stack<T> : List<T>
+    {
+        public void Push(T item) => Insert(0, item);
+
+        public T Pop()
+        {
+            if (Count <= 0) return default;
+
+            var item = this[0];
+            RemoveAt(0);
+            return item;
+        }
+
+        public T Peek()
+        {
+            return Count > 0 ? this[0] : default;
+        }
+    }
+}
---
+++ JsonChan/Json.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using Paraparty.JsonChan.Exceptions;
+
+namespace Paraparty.JsonChan
+{
+    /// <summary>
+    /// Static JSON parser class
+    /// </summary>
+    public static class Json
+    {
+        /// <summary>
+        /// Parse the JSON string to dynamic tree
+        /// </summary>
+        /// <param name="string"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidJsonException"></exception>
+        public static dynamic Parse(string @string)
+        {
+            // Parse to dynamic tree
+            var parser = new JsonParser(@string);
+            return parser.Parse();
+        }
+
+        /// <summary>
+        /// Parse the JSON stream to dynamic tree
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidJsonException"></exception>
+        public static dynamic Parse(Stream stream)
+        {
+            // Parse to dynamic tree
+            var parser = new JsonParser(stream);
+            return parser.Parse();
+        }
+    }
+}

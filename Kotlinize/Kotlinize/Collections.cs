ac50a03e4b45f0062604b708d4203805ed0c3eb2
Eric_Lian <git@superexercisebook.com>
2022-02-21 14:03:00 +0800

    feat: Collection utils

--- Kotlinize/Kotlinize/Collections.cs
+++ Kotlinize/Kotlinize/Collections.cs
@@ -1,17 +1,91 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Paraparty.Kotlinize
 {
     public static class Collections
     {
         /// <summary>
-        /// Create a list by given item.
+        /// Create a list by given item. <br/>
+        ///
+        /// This function is equivalent to linq expression <code>items.ToList()</code> .
         /// </summary>
         /// <param name="items"></param>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static List<T> ListOf<T>(params T[] items)
-            => new List<T>().Also(s => s.AddRange(items));
+        {
+            var ret = new List<T>();
+            ret.AddRange(items);
+            return ret;
+        }
 
+        /// <summary>
+        /// Create a KeyValuePair by given key and value. <br/>
+        ///
+        /// This function is equivalent to linq expression <code>new KeyValuePair&lt;TKey, TValue&gt;(key, value)</code> .
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static KeyValuePair<TKey, TValue> To<TKey, TValue>(this TKey key, TValue value)
+            => new KeyValuePair<TKey, TValue>(key, value);
+
+        /// <summary>
+        /// Create a KeyValuePair by given key and value. <br/>
+        ///
+        /// This function is equivalent to linq expression <code>new KeyValuePair&lt;TKey, TValue&gt;(key, value)</code> .
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static KeyValuePair<TKey, TValue> PairOf<TKey, TValue>(TKey key, TValue value)
+            => new KeyValuePair<TKey, TValue>(key, value);
+
+        /// <summary>
+        /// Create a Dictionary by given items. <br/>
+        ///
+        /// This function is equivalent to linq expression <code>items.ToDictionary(s => s.Key, s => s.Item)</code> .
+        /// </summary>
+        /// <param name="items"></param>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Dictionary<TKey, TValue> MapOf<TKey, TValue>(params KeyValuePair<TKey, TValue>[] items)
+        {
+            var ret = new Dictionary<TKey, TValue>();
+            // ReSharper disable once LoopCanBeConvertedToQuery
+            foreach (var item in items)
+            {
+                ret.Add(item.Key, item.Value);
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// Create a Set by given items. <br/>
+        /// </summary>
+        /// <param name="items"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ISet<T> SetOf<T>(params T[] items)
+        {
+            var ret = new HashSet<T>();
+            foreach (var item in items)
+            {
+                ret.Add(item);
+            }
+            return ret;
+        }
     }
 }
--- Tests/Kotlinize/CollectionExtension.cs
+++ Tests/Kotlinize/CollectionExtension.cs
@@ -3,7 +3,6 @@ using Xunit;
 
 namespace Paraparty.Tests.Kotlinize;
 
-
 public class CollectionExtension
 {
     [Fact]
@@ -12,5 +11,35 @@ public class CollectionExtension
         var t = Collections.ListOf(1, 2.2);
         Assert.Equal(1, t[0]);
         Assert.Equal(2.2, t[1]);
+    }
+
+    [Fact]
+    public void CreatePair()
+    {
+        var t = "ΩΩPARTS".To("かめりあ");
+        Assert.Equal("ΩΩPARTS", t.Key);
+        Assert.Equal("かめりあ", t.Value);
+    }
+    
+    [Fact]
+    public void CreateMap()
+    {
+        var t = Collections.MapOf(
+            "ΩΩPARTS".To("かめりあ"),
+                        "ANiMA".To("Xi")
+        );
+        Assert.Equal("かめりあ", t["ΩΩPARTS"]);
+        Assert.Equal("Xi", t["ANiMA"]);
+    }
+    
+    [Fact]
+    public void CreateSet()
+    {
+        var t = Collections.SetOf(
+            1, 1, 4, 5, 1, 4
+        );
+        Assert.Equal(3,t.Count);
+        Assert.True(t.Contains(1));
+        Assert.False(t.Contains(2));
     }
 }

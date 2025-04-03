e4a0c0d4322fda078a3b8b130cb6babe7fa386c1
Eric_Lian <git@superexercisebook.com>
2025-04-03 16:59:39 +0800

    fix: polyfill

--- UnityPolyfill/UnityPolyfill/CollectionsPolyfill.cs
+++ UnityPolyfill/UnityPolyfill/CollectionsPolyfill.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
 namespace Paraparty.UnityPolyfill
@@ -8,7 +7,7 @@ namespace Paraparty.UnityPolyfill
     {
 #if UNITY_2020_1_OR_NEWER && !UNITY_2022_1_OR_NEWER
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool TryPop<T>(this Stack<T> self, [MaybeNullWhen(false)] out T result)
+        public static bool TryPop<T>(this Stack<T> self, /* [MaybeNullWhen(false)] */ out T result)
         {
             if (self.Count > 0)
             {

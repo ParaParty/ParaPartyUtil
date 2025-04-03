74ef5cd941080949933e537ca1afe319f42798b1
Buger404 <ris_vb@126.com>
2025-04-03 17:20:07 +0800

    feat: add dictionary polyfill

--- UnityPolyfill/UnityPolyfill/CollectionsPolyfill.cs
+++ UnityPolyfill/UnityPolyfill/CollectionsPolyfill.cs
@@ -20,6 +20,18 @@ namespace Paraparty.UnityPolyfill
                 return false;
             }
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool TryAdd<TKey, TValue>(this Dictionary<TKey, TValue> self, TKey key, TValue value)
+        {
+            if (self.ContainsKey(key))
+            {
+                return false;
+            }
+            
+            self.Add(key, value);
+            return true;
+        }
 #endif
     }
 }
